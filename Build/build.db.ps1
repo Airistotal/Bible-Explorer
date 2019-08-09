0c02aeec4292d0f9115d446a482e13c09d4fa3e3
Airistotal <danieltos.bouchard@gmail.com>
2019-08-09 17:47:41 -0500

    Changed DB name in script

--- Build/build.db.ps1
+++ Build/build.db.ps1
@@ -4,20 +4,20 @@ $srv = new-Object Microsoft.SqlServer.Management.Smo.Server("localhost")
 
 $dbExists = $FALSE
 foreach ($currDb in $srv.databases) {
-  if ($currDb.name -eq "BE") {
-    Write-Host "BE already exists."
+  if ($currDb.name -eq "BB") {
+    Write-Host "BB already exists."
     $db = $currDb
     $dbExists = $TRUE
   }
 }
 
 if ($dbExists -eq $FALSE) {
-  $db = New-Object Microsoft.SqlServer.Management.Smo.Database($srv, "BE")
+  $db = New-Object Microsoft.SqlServer.Management.Smo.Database($srv, "BB")
   $db.Create()
 }
 
 Get-ChildItem ".\Database\tsql" -Filter *.sql | 
 Foreach-Object {
   Write-Host "Running script" $_.Name
-  Invoke-Sqlcmd -Inputfile $_.FullName -ServerInstance localhost -Database BE
+  Invoke-Sqlcmd -Inputfile $_.FullName -ServerInstance localhost -Database BB
 }
