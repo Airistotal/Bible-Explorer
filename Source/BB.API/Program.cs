3dcfb583ffa9a8ce941a7590c75bdb3d3295ff44
Airistotal <danieltos.bouchard@gmail.com>
2019-11-12 22:09:04 -0600

    Upgrade to .net core 3.0

--- Source/BB.API/Program.cs
+++ Source/BB.API/Program.cs
@@ -1,26 +1,29 @@
-﻿using System.IO;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace BE.API
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-           WebHost.CreateDefaultBuilder(args)
-               .ConfigureAppConfiguration((hostingContext, config) =>
-               {
-                   config.SetBasePath(Directory.GetCurrentDirectory());
-                   config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                   config.AddEnvironmentVariables();
-                   config.AddCommandLine(args);
-               })
-               .UseStartup<Startup>();
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+              config.SetBasePath(Directory.GetCurrentDirectory());
+              config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+              config.AddEnvironmentVariables();
+              config.AddCommandLine(args);
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
