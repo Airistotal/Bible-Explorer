853f2b5e8942c9af09913a6b44dabcf073b63d38
Daniel <danieltos.bouchard@gmail.com>
2019-11-14 22:52:04 -0600

    Circle ci setup

--- Source/BB.API/Startup.cs
+++ Source/BB.API/Startup.cs
@@ -3,69 +3,59 @@ using BB.Infrastructure.Context;
 using BB.Infrastructure.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
+using Microsoft.Extensions.Hosting;
 
 namespace BE.API
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            this.Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            var bbConnectionString = this.Configuration.GetConnectionString("BB");
-            services.AddDbContext<BibleContext>(options => options.UseSqlServer(bbConnectionString));
-
-            this.InitializeIOC(services);
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseDefaultFiles(new DefaultFilesOptions
-            {
-                DefaultFileNames = new List<string> { "index.html" }
-            });
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-            app.UseMvc();
-        }
-
-        public void InitializeIOC(IServiceCollection services)
-        {
-            services.AddTransient<IBibleService, BibleService>();
-            services.AddTransient<ITextDiff, TextDiff>();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+
+      this.InitializeIOC(services);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseStaticFiles();
+      app.UseDefaultFiles();
+
+      app.UseHttpsRedirection();
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+
+    public void InitializeIOC(IServiceCollection services)
+    {
+      var bbConnectionString = this.Configuration.GetConnectionString("BB");
+      services.AddDbContext<BibleContext>(options => options.UseSqlServer(bbConnectionString));
+
+      services.AddTransient<IBibleService, BibleService>();
+      services.AddTransient<ITextDiff, TextDiff>();
+    }
+  }
 }
